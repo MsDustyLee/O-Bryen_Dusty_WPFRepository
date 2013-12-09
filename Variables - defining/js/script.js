549f133933497db84375d1f415be7a9cb999dfe1
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-08 20:26:50 -0800

    Following along with strings and boolean variables screencast.

--- Variables - defining/js/script.js
+++ Variables - defining/js/script.js
@@ -1,3 +1,11 @@
 /**
  * Created by msdustylee on 12/8/13.
  */
+//Defining Variables
+
+var age; //declare means to name the variable
+age = 39; //define the variable means to assign it a value
+//storage container on left = value on the right
+
+var yearOfBirth = 1974; //declaring and defining on same line
+
---
+++ Variables - strings boolean/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by msdustylee on 12/8/13.
+ */
+//Variables of different types, numbers, booleans, and strings
+
+var age = 39; //number variable
+
+var isStudent = true; //boolean variable is going to be either true or false
+
+var firstName = "Dusty"; //string variable encased with double quotations
+
+//back slash tells computer to ignore the character that it's in front of
+//forward slash tells computer to
+// \n starts a new line within a string
