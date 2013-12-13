db70095b7c260326f5e107d0c8853b9e340e3501
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-12 16:46:41 -0800

    I had this information in Git before already, but I accidentally deleted this information last night when I changed the names of my folders in my repository stored on my computer.  I’m not quite sure yet how I did this, but I just re-entered previous notes and I’m resubmitting it.  This is notes taken from following along with the defining variables screen cast.

---
+++ variables_defining/js/script.js
@@ -0,0 +1,16 @@
+/* Defining Variables means to give it a value.
+First you declare it by naming it, then you define it by giving it value.
+
+
+ Storage container is on left = value is on the right */
+
+
+var age;  //declaration
+age = 39;  //definition
+
+//Can also define and declare on the same line as below.
+
+var yearOfBirth = 1974;  //declaring and defining
+
+//always declare first and THEN define.  Cannot switch order.
+
