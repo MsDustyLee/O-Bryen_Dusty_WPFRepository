4c01393c8ba597c8d24358eb981a9f526ec59045
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-12 17:02:54 -0800

    Added more notes after rewatching the screencast about console log and alerts.

--- Values_and_Variables/js/script.js
+++ Values_and_Variables/js/script.js
@@ -9,3 +9,10 @@ console.log("Hello World");
 */
 
 alert(yearBorn);
+
+/* The point of the console is so that we can see what the value is of the variable.
+The console log and alerts are funtions.
+The point of the alert function is to pop up an
+alert of whatever we entered in the parenthesis.
+ */
+
