2d4701daf3b4ea81d7ff5d66b3ded9d5d079f20a
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-14 14:30:17 -0800

    Following along with basic expressions screen cast, taking notes.

---
+++ Expressions/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Created by msdustylee on 12/14/13.
+ */
+//EXPRESSIONS
+
+/* var a = 2;
+console.log (a);  meant to check a's value in the console, to test it
+it worked
+now try to add
+a + 3
+this will not equal 5 bc there is no storage container to change a's new value to 5
+below is the correct format to hold the value of 5 */
+
+var a = 2;
+a = a + 3; //this changed the value of a from 2 to a new value of 5
+
+console.log (a);
+
+//or
+
+var a = 2;
+var b = a + 3;
+console.log (b); //this makes the value of b 5
+console.log (a); //the value of a will still be 2
