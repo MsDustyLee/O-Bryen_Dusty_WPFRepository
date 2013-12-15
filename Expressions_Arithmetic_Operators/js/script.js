8d7883deb08daf26d24cdc46b20e98b9398b5752
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 13:02:22 -0800

    Following along with Arithmetic Operators screencast.

---
+++ Expressions_Arithmetic_Operators/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by msdustylee on 12/14/13.
+ */
+//EXPRESSIONS - ARITHMETIC OPERATORS
+
+//area of a triangle is half of the width times the height
+
+var width = 4;
+var height = 5;
+var area = width * height / 2;
+/* using the names of the variables means we can change the value
+of width and the value of height at any time, and still make
+the variable area correct.  If we had used:
+
+var area = 4*5/2;
+
+then we would have also had to go in to change the values in the declaration
+of area as well.  Just takes more time and leaves more room for error.
+ */
+
+console.log(area);
+
