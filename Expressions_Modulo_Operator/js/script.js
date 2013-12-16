db3b5be9b16ffc52a0969370f5523aa9892da5fb
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 16:25:09 -0800

    Following Modulo Operator screencast

---
+++ Expressions_Modulo_Operator/js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by msdustylee on 12/15/13.
+ */
+//Expressions - Modulo Operator
+
+/* If we coded our expression as 32 divided by 10:
+
+var remainder = 32 / 10;
+console.log(remainder)
+
+we would get an answer of 3.2, which can be seen on the console.
+
+But, to get the actual remainder we need to use the percent symbol.
+ */
+
+var remainder = 32 % 10;
+console.log(remainder);
+
