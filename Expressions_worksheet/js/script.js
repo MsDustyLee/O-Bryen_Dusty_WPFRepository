f1544d362749d834795ff7ae8dec0563e91c8030
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-16 01:28:30 -0800

    Answer 3 took me forever to figure out.  I put in what I thought was the correct expression to find out how many pieces of pizza Sparky got, but when I tested it on the console it kept coming up with an alert: SyntaxError: Unexpected token ‘*’.  I then changed the expression 6 more times and still kept getting the error.  I got frustrated and took a break from it to think; and I went back in to study the console results closely, and then I realized, all the errors ended with ‘*’, telling me that I had an extra astric that didn’t belong, and then I realized off to the right that the console told me which line the error was on.  As soon as I removed the astric I went back in to put the original expression in that I had tried and it worked correctly and with the right answer.  I then took it a step further to write out another longer expression to show how I tested my answer.

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,7 +9,7 @@ Dusty O'Bryen
 */
 
 //Calculate Sparky's age in dog years
- */
+
 var sparkyAge = 5;
 const dog = 7;
 
@@ -17,10 +17,11 @@ const dog = 7;
 var dogYears = sparkyAge * dog;
 
 //Concatenate strings and variables together and assign to answer variable
-var answer = "Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years.";
+var answer = "Answer 1 is: " + "Sparky is " + sparkyAge + " human years old which is " + dogYears + " in dog years.";
 
 //print out the output of the variable answer
-console.log(answer);
+console.log(answer); //35
+
 
 
 
@@ -37,7 +38,50 @@ var pizzaOrdered = 6;
 var slicesPP = slices * pizzaOrdered / partyGoer;
 
 //Concatenate strings and variables together and assign to answer2 variable
-var answer2 = "Each person ate " + slicesPP + " slices of pizza at the party.";
+var answer2 = "Answer 2 is: " + "Each person ate " + slicesPP + " slices of pizza at the party.";
 
 //print out the output of the variable answer2
-console.log(answer2);
+console.log(answer2); //4.8
+
+
+
+/* Slice of Pie part II
+    */
+
+//Use variables from Slice of Pie 1 to figure out the amount of pizza Sparky gets to eat
+
+/*Use the same expression as slicesPP, but use the modulo operator
+in place of division operator in order to figure out remaining amount.
+*/
+remainder = slices * pizzaOrdered % partyGoer;
+
+//Concatenate strings and variables together and assign to answer3 variable
+var answer3 = "Answer 3 is: " + "Sparky got " + remainder + " slices of pizza.";
+
+//print out the output of the variable answer3
+console.log(answer3);
+
+/* To test my answer I changed the expression around:
+Based on answer2's results, we know that there is at least
+4 whole slices of pizza for each person.  We need to figure the
+total amount of slices of pizza, then divide by the whole number 4.
+That gives us the answer for the amount of people the pizza would feed
+if everyone only had 4 slices each.  Then we subtract the amount of people
+that are actually there.  We take that that number and turn it back into
+ the amount of pizza slices are left.  If that number is higher than the
+ amount of people at the party, then we know we got our whole number wrong.
+ If that amount is less than the number of people there, then we know there
+ wasn't enough left to be split up between all the people evenly, so that
+ remaining amount was given to Sparky.
+
+ Here's how I tested it:
+ */
+
+var totalPizza = slices * pizzaOrdered;
+var each = 4;
+var enoughFor = totalPizza / each;
+console.log("Enough for " + enoughFor + " people, but we only have " + partyGoer + " people attending."); //Enough for 18 people
+
+remainder = (enoughFor - partyGoer) * each;
+answer3 = "Answer 3 is: " + "Sparky got " + remainder + " slices of pizza.";
+console.log(answer3);
