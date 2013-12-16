38d37a20e0a06ac22d18e766d600824655370c14
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 20:20:29 -0800

    Following along with Assignment Operators screencast.

---
+++ Assignment_Operators/js/script.js
@@ -0,0 +1,38 @@
+/**
+ * Created by msdustylee on 12/15/13.
+ */
+//Asignment Operators - an expression with assignment
+
+//Operators are: =, +=, *=, /*, -+, ++, --
+
+    /*
+var a = 3;
+a += 4; //is the same as: a = a + 4;
+console.log(a);
+
+
+     var a = 3;
+     a *= 4; //is the same as: a = a * 4;
+     console.log(a);
+
+
+     var a = 3;
+     a /= 4; //is the same as: a = a / 4;
+     console.log(a);
+
+
+     var a = 3;
+     a -= 4; //is the same as: a = a - 4;
+     console.log(a);
+
+
+     var a = 3;
+     a ++; //is the same as: a = a + 1;
+     console.log(a);
+
+
+     var a = 3;
+     a --; //is the same as: a = a - 1;
+     console.log(a);
+
+*/
