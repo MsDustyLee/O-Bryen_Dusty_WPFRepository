535b11d79dfa68df12232a8809ae9b6505dec910
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 19:12:02 -0800

    Following along with Concatenating Strings screencast

---
+++ Concatenating_Strings/js/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by msdustylee on 12/15/13.
+ */
+//Concatenating - Combining Strings
+    //strings are for holding characters
+
+    /*
+var firstName = "Dusty";
+var middleName = "Lee";
+var lastName = "O'Bryen-Milks"
+var fullName = firstName + " " + middleName + " " + lastName;
+console.log(fullName);
+*/
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
+
+//shows as 67, not the value 13
+
