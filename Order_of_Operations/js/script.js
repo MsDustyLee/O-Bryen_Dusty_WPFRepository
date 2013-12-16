bd1801f70f0b7c10f9a019e1e1d0b994482754c9
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 17:03:23 -0800

    Following Order of Operations screencast

---
+++ Order_of_Operations/js/script.js
@@ -0,0 +1,33 @@
+/**
+ * Created by msdustylee on 12/15/13.
+ */
+//Expressions - PEMDAS: Order of Operations - order of the math processes
+    /* Math Order is:
+    Parenthesis
+    Exponents
+    Multiplication
+    Division
+    Addition
+    Subtraction
+
+    "PLEASE EXCUSE MY DEAR AUNT SALLY" to remember the order
+     */
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//to get the average of a series of numbers:
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4) /4;
+console.log(average);
+
+//we need to put parenthesis around the numbers we need added together first
+
+var length = 7;
+var width = 6;
+var perimeter = length *2 + width *2;
+//no perenthesis needed because the multiplication already happens first.
+
+console.log(perimeter);
