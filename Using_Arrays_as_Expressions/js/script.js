a289a5f5df0d8cd8afa024fd6cfe2b837826632b
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-15 18:18:06 -0800

    Following along with Using Arrays as Expressions screencast

---
+++ Using_Arrays_as_Expressions/js/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by msdustylee on 12/15/13.
+ */
+//Using Arrays As Expressions
+
+var orangeBins = [234, 567, 883];
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
