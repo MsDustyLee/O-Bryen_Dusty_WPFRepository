9e766cea554427c65c075ba383fb369b9058bc14
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-18 20:23:27 -0800

    Following along with conditionals logic screen cast

---
+++ Conditionals/js/script.js
@@ -0,0 +1,15 @@
+//CONDITIONAL LOGIC
+
+var oldEnough = true;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+    console.log("You can ride the roller coaster!");
+
+} //code performed if condition is true
+
+/*if the var oldEnough were false, meaning not old enough, then it will skip
+the information going to the console.
+ */
+
