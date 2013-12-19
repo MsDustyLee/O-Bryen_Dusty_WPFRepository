09343b323f51bf0a10146ac02490f8bbd430537f
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-18 20:57:14 -0800

    Following along with Relational Expressions Screencast

---
+++ Relational_Expressions/js/script.js
@@ -0,0 +1,17 @@
+//Relational Expressions - Comparing objects for decision making
+
+var kidHeight = 40;
+var minHeight = 48;
+
+//if the kid is over 48" in height
+
+if(kidHeight > 48){
+    console.log("You can ride the roller coaster!");
+}
+
+/* or,
+
+if(kidHeight > minHeight){action}
+
+ */
+
