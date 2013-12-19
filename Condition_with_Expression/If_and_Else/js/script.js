f89aca49acbc096a6ee9312f0f8247116e36c7b0
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-18 21:27:56 -0800

    Following along with If and Else screencast

---
+++ Condition_with_Expression/If_and_Else/js/script.js
@@ -0,0 +1,44 @@
+//Condition with Expressions - If and Else: Choosing between two blocks of code
+
+/*
+
+var kidHeight = 47;
+var minHeight = 48;
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the roller coaster!");
+}
+
+if(kidHeight <= minHeight){
+    //code performed if condition is false
+    console.log("Sorry, you've got some growing to do!");
+}
+
+Or,
+
+ var kidHeight = 48;
+ var minHeight = 48;
+
+ if(kidHeight > minHeight){
+ console.log("You can ride the roller coaster!");
+ }
+
+ if(kidHeight <= minHeight){
+ console.log("Sorry, you've got some growing to do!");
+ }
+
+ */
+
+
+//Can do the expression as the forms above, but the easier way is:
+
+var kidHeight = 47;
+var minHeight = 48;
+
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the roller coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry, you've got some growing to do!");
