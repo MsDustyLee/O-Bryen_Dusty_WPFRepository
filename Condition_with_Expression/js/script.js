71cdef3470b75fce2c7cfa090d5f39f56a1a98cc
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-18 21:07:45 -0800

    Following along with Conditions with Expressions Screencast.

---
+++ Condition_with_Expression/js/script.js
@@ -0,0 +1,17 @@
+//Condition with Expressions
+
+var kidHeight = 47;
+var minHeight = 48;
+
+//if the kid is over 48" in height
+
+if(kidHeight +2 > minHeight){
+    console.log("You can ride the roller coaster!");
+}
+
+/* or:
+
+var sneakerLift = 2
+
+if(kidHeight + sneakerLift > minHeight){action}
+ */
