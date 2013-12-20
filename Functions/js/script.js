a633e18d65b4ab7a060c58b19284ab9276a63ff4
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-19 19:49:09 -0800

    Followed Basic Functions screencast.

---
+++ Functions/js/script.js
@@ -0,0 +1,28 @@
+//Functions - Store blocks of code
+
+/*
+TYPES:
+Frunctions - "normal"
+Procedures
+Anonymous Functions
+ */
+
+/*
+BASIC FUNCTIONS:
+
+function functionName(){
+    // Code the function runs
+
+    EXAMPLES:
+    */
+
+    function outptMsg(){
+    console.log("Hello World");
+    }
+
+ function calcArea(){
+ var width = 20;
+ var height = 30;
+ var area = width * height;
+ console.log(area);
+}
