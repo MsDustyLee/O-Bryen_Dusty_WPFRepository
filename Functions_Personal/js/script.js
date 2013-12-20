73edc3e0d218b5259909ebc37b8ef8e2d2fd1db5
Dusty O'Bryen <MsDustyLee@fullsail.edu>
2013-12-19 23:17:35 -0800

    I am creating a prompt to collect the number of students in the class, and the variable studentCount will hold the value of the user’s input.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -5,3 +5,28 @@
  */
 
 
+/*
+
+ Use a function as part of your code in each sub-project. This function should have at least two parameters and return a value.
+
+ Each of the following requirements must exist in at least one of the 3 subprojects:
+ A ternary
+ An else if
+ An anonymous function
+ A normal "named" function
+ An expression with two arithmetic operators
+ A function with three parameters.
+ At least one logical operator
+
+ */
+
+/*
+My son was assigned the weekly snack for his entire class.
+I need to find out how many students are in the class.  And I
+need to decide what snacks to get; and calculate how much it
+is going to cost to purchase them.
+ */
+
+var studentCount = prompt("How many students are in your classroom?");
+
+
