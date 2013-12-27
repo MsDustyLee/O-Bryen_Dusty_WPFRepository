0b6e4e06b140c694d0a02aab52e5a6db5980063f
josephdalton <jdalton@fullsail.com>
2013-12-27 14:54:19 -0500

    JMD Comments
    Dusty,
    The code presented has some fairly significant issues with encapsulation, variable creation/assignment, and use of conditionals. I have rewritten the code as an example. Please let me know if you have any questions.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -21,43 +21,95 @@
  */
 
 /*
-My son was assigned the weekly snack for his entire class.
-I need to find out how many students are in the class.  And I
-need to decide what snacks to get; and calculate how much it
-is going to cost to purchase them.
+ My son was assigned the weekly snack for his entire class.
+ I need to find out how many students are in the class.  And I
+ need to decide what snacks to get; and calculate how much it
+ is going to cost to purchase them.
  */
 
 
+/*
+ weeklySnack(studentCount, allergy);
 
-weeklySnack(studentCount, allergy);
+ function weeklySnack(studentCount, allergy){
+ var studentCount = prompt("How many students are in your classroom?");
+ return studentCount;
+ var allergy = prompt("Do any of your students have any dairy allergies? \n YES or NO");
 
-function weeklySnack(studentCount, allergy){
-    var studentCount = prompt("How many students are in your classroom?");
-    return studentCount;
-    var allergy = prompt("Do any of your students have any dairy allergies? \n YES or NO");
+ if(allergy = yes){snack1;
+ }else{snack2;
+ }
+ return allergy;
 
-    if(allergy = yes){snack1;
-   }else{snack2;
+ }
+
+ console.log(weeklySnack);
+
+
+ var snack1 = "Crackers, Cheese & Sausage Packs";
+ var snack1Cost = 2.49 * studentCount;
+ var yes = "yes" || "YES";
+ yes = true;
+ var a = "Yes" == yes;
+ console.log("I will be sending along " + snack1 + ".  I hope your students enjoy it.");
+ alert("I will be sending along " + snack1 + ".  I hope your students enjoy it.");
+
+
+
+ var snack2 = "Still trying to decide.";
+ var snack2Cost = 1.25 + studentCount;
+ var no = "no" || "NO";
+ no = false;
+ var b = "No" == no;
+
+ */
+
+
+//Dusty, the solution provided has a few issues. Here is how I would address the problem.
+//GENERAL COMMENTS WILL BE PLACED TO PROVIDE INFORMATION ABOUT THE CODE.
+//[ SQUARE BRACKETS WILL BE USED TO PROVIDE COMMENTARY FOR YOU]
+
+//Purpose: I am going to build a cost calculator for assessing the cost of bringing snacks to a class.
+//I will be passing in an array 2 dimensional containing the names of the students and their diet concerns. [I AM USING AN ARRAY BECAUSE IT WILL ALLOW FOR STUDENT ROSTERS TO BE ASSEMBLED (AND IT IS A LITTLE MORE COMPLEX SO YOU CAN SEE HOW TO DO IT)]
+//I will loop through the array and calculate the cost of feeding the class each snack each day.
+
+
+
+function classCalculator(classArray){
+var finalValueOfClass=0;    //this is where the final value will be stored, but to start we need it set to ZERO.
+
+//[to collect the values from the array studentList I need to look at each value and decide what to do with them.  ]
+//[so I have to loop through the array and i do that with a for loop]
+    for(i=0;i<classArray.length;i++){
+    if(classArray[i][1]==true){     //[This line says to check the second second value (the true/false value) to see if it is true. Remember arrays start at 0 so 1 is the second value
+        finalValueOfClass+=2.49; //add the value of a full price snack to the running total;
+
+    } else {
+        finalValueOfClass+=1.25;  //add the discounted value to the running total.
     }
-    return allergy;
-
 }
-
-console.log(weeklySnack);
-
-
-var snack1 = "Crackers, Cheese & Sausage Packs";
-var snack1Cost = 2.49 * studentCount;
-var yes = "yes" || "YES";
-yes = true;
-var a = "Yes" == yes;
-console.log("I will be sending along " + snack1 + ".  I hope your students enjoy it.");
-alert("I will be sending along " + snack1 + ".  I hope your students enjoy it.");
+  return finalValueOfClass;   //once the values have been added I have to return the values.
+ }
 
 
+//array studentList[NAME][DIET CONSTRAINT]
+var studentList =[];
+studentList[0]=['george',true];
+studentList[1]=['verna',false];
+studentList[2]=['mary',true];
+studentList[3]=['robert',false];
+studentList[4]=['alice',true];
+studentList[5]=['martin',false];
+//Here is my array with values
 
-var snack2 = "Still trying to decide.";
-var snack2Cost = 1.25 + studentCount;
-var no = "no" || "NO";
-no = false;
-var b = "No" == no;
+
+console.log(classCalculator(studentList));
+
+
+
+
+
+
+
+
+
